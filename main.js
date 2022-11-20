25b348efa10fb4a28185cebea3f099744b05dd62
Bruno Antonio <68406566+antbsp@users.noreply.github.com>
2022-11-20 17:56:07 -0300

    Update main.js

--- main.js
+++ main.js
@@ -8,3 +8,9 @@ $(".modal-close").click(function() {
     $(this).parents().eq(2).toggleClass("modal-container-open");
     $(this).parents().eq(1).toggleClass("modal-open");
 });
+
+$(document).ready(function() {
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        alert("A visualização em dispositivos móveis pode ser prejudicada pelo tamanho da tela. Se possível, utilize um computador para acessar o site.");
+    }
+});
