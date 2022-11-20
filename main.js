94069f1f7b818b290be5ae7aad613dc9f13243ca
Bruno Antonio <68406566+antbsp@users.noreply.github.com>
2022-11-20 19:08:12 -0300

    Update main.js

--- main.js
+++ main.js
@@ -8,10 +8,3 @@ $(".modal-close").click(function() {
     $(this).parents().eq(2).toggleClass("modal-container-open");
     $(this).parents().eq(1).toggleClass("modal-open");
 });
-
-$(document).ready(function() {
-    var width = $(document).outerWidth();
-    if(width < 700) {
-        alert("A visualização do site em dispositivos móveis pode ser prejudicada. Se possível, utilize um computador.");
-    }
-});
